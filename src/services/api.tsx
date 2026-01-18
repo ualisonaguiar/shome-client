f29a3c5b33121ab8098e961938afc2cc62c2f023
Ualison Aguiar <ualison.aguiar@gmail.com>
2026-01-18 00:05:46 -0300

    alterando o nome de const

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import AuthenticatorService from "./AuthenticatorService";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.API_URL,
   headers: {
     "Content-Type": "application/json",
     "Cache-Control": "no-cache",
